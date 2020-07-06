c362592b00d010c5db83d3f98b29958955b79810
Idan <idan.zonshein@powerhrg.com>
2020-07-06 11:27:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards( all_names, event ) {
+  let cards = []
+  for ( let i = 0; i < all_names.length; i++ ) {
+    cards.push( `Thank you, ${all_names[i]}, for the wonderful ${event} gift!` )
+  }
+  return cards
+}
+
+function countDown( countdown ) {
+  while ( countdown > 0 ) {
+    console.log( countdown );
+    countdown -= 1;
+  }
+  console.log( countdown );
+}
